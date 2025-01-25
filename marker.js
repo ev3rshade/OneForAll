204632c33c8602c35edfe57f93c479ee3f5fb693
lucy-x <lucyclxu@gmail.com>
2025-01-25 15:52:26 -0500

    separated code into js and html

--- marker.js
+++ marker.js
@@ -1,27 +1,50 @@
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+function getlocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showLoc, errHand);
+    }
+}
+function showLoc(pos) {
+    latt = pos.coords.latitude;
+    long = pos.coords.longitude;
+    var lattlong = new google.maps.LatLng(latt, long);
+    var OPTions = {
+        center: lattlong,
+        zoom: 10,
+        mapTypeControl: true,
+        navigationControlOptions: {
+            style: google.maps.NavigationControlStyle.SMALL,
+        },
+    };
+    var mapg = new google.maps.Map(
+        document.getElementById("demo2"),
+        OPTions
+    );
+    var markerg = new google.maps.Marker({
+        position: lattlong,
+        map: mapg,
+        title: "You are here!",
+    });
 }
 
-initMap();
+function errHand(err) {
+    switch (err.code) {
+        case err.PERMISSION_DENIED:
+            result.innerHTML =
+                "The application doesn't have the permission" +
+                "to make use of location services";
+            break;
+        case err.POSITION_UNAVAILABLE:
+            result.innerHTML = 
+                  "The location of the device is uncertain";
+            break;
+        case err.TIMEOUT:
+            result.innerHTML = 
+                  "The request to get user location timed out";
+            break;
+        case err.UNKNOWN_ERROR:
+            result.innerHTML =
+                "Time to fetch location information exceeded" +
+                "the maximum timeout interval";
+            break;
+    }
+}
